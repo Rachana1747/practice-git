554a2f4170bc58ed4a337ddcf56f1c62a859aa97
Rachana1747 <rachanap2803@gmail.com>
2025-03-05 15:43:34 +0530

    Add exx file

---
+++ exx.js
@@ -0,0 +1,41 @@
+// function Employee(name,a,e){
+//     this.name=name;
+//     var age=a;
+//     let email=email;
+//     function print1(){
+//         console.log(this.name);
+//     }
+// }
+
+// let empobj=new Employee("abc",10,"abc@email.com");
+
+// Employee.prototype.print4=function(){
+//     console.log(this.name);
+// }
+
+// let x=10;
+// console.log(x);
+// x=4;
+// console.log(x);
+
+// var a;
+// console.log(a);
+// a=7;
+
+// var fun=function (){
+//     console.log("hello");
+// }
+
+console.log(add(2,4));
+function add(a,b){
+    return a+b;
+}
+
+console.log(add);
+const add=function(a,b){
+    return a+b;
+}
+
+
+
+
