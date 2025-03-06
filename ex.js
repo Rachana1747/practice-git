f2409cc3cff318dc84531c528984fe2a74ec831b
Rachana1747 <rachanap2803@gmail.com>
2025-03-06 12:51:10 +0530

    Modified file

--- ex.js
+++ ex.js
@@ -2,4 +2,11 @@ console.log("Hello");
 function add(a,b){
     return a+b;
 }
+<<<<<<< Updated upstream
 console.log(add(10,20));
+=======
+console.log(add(10,20));
+
+console.log("This is created for Undoing changes");
+console.log("implementing stashing")
+>>>>>>> Stashed changes
